2c3f20a49b4af8057b92d3f5352964d81a2d40b1
Kim Richards <richards.kimc@gmail.com>
2025-09-20 12:19:23 -0400

    first commit

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,32 @@
+import { type PropsWithChildren } from "react";
+
+
+type CourseGoalProps = PropsWithChildren<{title: string}>;
+
+export default function CourseGoal({title, children}: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+          <h2>{title}</h2>
+          <p>{children}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
+
+// const CourseGoal: FC<CourseGoalProps> = ({title, children} ) => {
+//     return (
+//         <article>
+//           <div>
+//               <h2>{title}</h2>
+//               {children}
+//           </div>
+//           <button>Delete</button>
+//         </article>
+//       );
+// }
+
+// export default CourseGoal;
+
+
